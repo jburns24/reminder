b33b17b6928debe794c819132baaa4ff05887fbb
Joshua Burns <joshuajohnburns@gmail.com>
2019-01-08 20:23:09 -0800

    simplified args, parsed out number of seconds to wait, and got a working python daemon program that can send to gnome notifications

--- remind.py
+++ remind.py
@@ -15,11 +15,11 @@ def cleanUp():
 # time delimiter is located so the message we want to 
 # remind the user about is located from arglist[1: index -1]
 def getTimeIndex(argList):
-    timeDelimeters = ["in", "at", "on"]
+    timeDelimeters = ['in', 'at', 'on']
     reversed_argList = list(reversed(argList))
     ti_index = []
-    if "every" in argList:
-        ti_index.append(reversed_argList.index("every"))
+    if 'every' in argList:
+        ti_index.append(reversed_argList.index('every'))
         if ti_index:
             return len(argList) - max(ti_index)        
     else:
@@ -29,8 +29,40 @@ def getTimeIndex(argList):
         if ti_index:
             return len(argList) - min(ti_index)
 
-def getTimePairs(time_str):
-    print(time_str)
+# This function takes a time string for the delimeter 'in'
+# and splits it into a list of 2-tuples. Each tuple represents
+# an amount of time. 
+def getTimePairs_in(time_str):
+    ti_list = time_str.split()
+    ti_list.remove('and')
+    if (len(ti_list) % 2) != 0:
+        print("You had malformatted time string for in. Accept input like '1 hour and 15 min")
+        exit()
+    tup_list = []
+    for i in range(len(ti_list) / 2):
+        index = i * 2
+        temp_list = [ti_list[index], ti_list[index + 1]]
+        tup_list.append(tuple(temp_list))
+    return tup_list
+
+def getTotalSeconds(time_tuple):
+    minutes = ['min', 'minutes', 'm']
+    hours = ['hours', 'hour', 'h']
+    seconds = ['seconds', 'sec', 's']
+    total_time = 0
+    for tu in time_tuple:
+        measurment = str(tu[1]).lower()
+        if measurment in minutes:
+            total_time += int(tu[0]) * 60
+            continue
+        if measurment in hours:
+            total_time += int(tu[0]) * 60 * 60
+            continue
+        if measurment in seconds:
+            total_time += int(tu[0])
+            continue
+    return total_time
+
 
 
 if __name__ == '__main__':
@@ -38,22 +70,23 @@ if __name__ == '__main__':
     numArgs = len(sys.argv)
     argList = sys.argv
     if numArgs == 0:
-        print("no args")
+        print('no args')
         exit()
     time_delimeter = getTimeIndex(argList)
     if time_delimeter is None:
-        message = " ".join(argList)
-        print(f'"{message}" is an invalid reminder')
+        message = ' '.join(argList)
+        print('Entered an invalid reminder')
         exit()
-    time_str = " ".join(argList[time_delimeter:])
-    message_str = " ".join(argList[1:time_delimeter - 1])
+    time_str = ' '.join(argList[time_delimeter:])
+    message_str = ' '.join(argList[1:time_delimeter - 1])
     delimeter = argList[time_delimeter - 1]
-    if delimeter = 'on':
-        getTimePairs(time_str)
-        exit()    
+    if delimeter == 'in':
+        time_list = getTimePairs_in(time_str)
+        total_time = getTotalSeconds(time_list)
+        exit()
     if min:
         #time.sleep(min[0])
-        Notify.init("Remind Me")
-        notification = Notify.Notification.new("REMINDER", str(argList))
+        Notify.init('Remind Me')
+        notification = Notify.Notification.new('REMINDER', str(argList))
         notification.show()
 
